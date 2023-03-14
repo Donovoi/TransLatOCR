40fefb6ae2e0ac852c6ff9465fd582e626b57dbe
Michael Moran <michael@theablecoder.io>
2023-03-15 04:31:30 +1100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+
+
+// This an application that acts similar to a screen reader, but instead will translate the text to a chosen language, allowing the user to copy paste the translation or simply view it in place of the original text
+// This is a work in progress, and is not yet functional
+
+
+fn main() {
+   // First we will make sure we are running with admin privileges, so we can view and interact with the screen
+
+}
