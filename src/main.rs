43272c833585f34904bb2baba2b3639291b47cf5
Michael Moran <michael@theablecoder.io>
2023-03-18 02:42:24 +1100

    blueprint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,119 @@
+use std::error::Error;
+use scrap::Capturer;
+use tesseract::TessApi;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use winit::{event::Event, event_loop::ControlFlow};
+
+// Struct for Google Translate API response
+#[derive(Debug, Deserialize, Serialize)]
+struct ApiResponse {
+    data: Data,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Data {
+    translations: Vec<Translation>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Translation {
+    translated_text: String,
+}
+
+// Function to translate the text using Google Translate API
+async fn translate_text(client: &Client, text: &str, api_key: &str) -> Result<String, Box<dyn Error>> {
+    let url = format!(
+        "https://translation.googleapis.com/language/translate/v2?key={}",
+        api_key
+    );
+
+    let params = [
+        ("q", text),
+        ("target", "en"),
+        ("format", "text"),
+        ("source", "auto"),
+    ];
+
+    let res: ApiResponse = client.post(&url).form(&params).send().await?.json().await?;
+    let translation = res.data.translations[0].translated_text.clone();
+
+    Ok(translation)
+}
 
 
-// This an application that acts similar to a screen reader, but instead will translate the text to a chosen language, allowing the user to copy paste the translation or simply view it in place of the original text
-// This is a work in progress, and is not yet functional
 
 
-fn main() {
-   // First we will make sure we are running with admin privileges, so we can view and interact with the screen
+// Main function
+async fn main() -> Result<(), Box<dyn Error>> {
+    let api_key = "YOUR_GOOGLE_TRANSLATE_API_KEY";
 
+    // Initialize the required libraries and APIs
+    let capturer = Capturer::new(0)?;
+let device = capturer.device();
+let format = capturer.format();
+    let tess_api = TessApi::new(None, "eng")?;
+    let client = Client::new();
+
+    // Initialize the window overlay using winit and wgpu
+    // ...
+
+    // Main event loop
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent { event, .. } => {
+                match event 
+                            // Handle window events like closing, resizing, etc.
+                            {
+                              winit::event::WindowEvent::Resized(_) => todo!(),
+                              winit::event::WindowEvent::Moved(_) => todo!(),
+                              winit::event::WindowEvent::CloseRequested => todo!(),
+                              winit::event::WindowEvent::Destroyed => todo!(),
+                              winit::event::WindowEvent::DroppedFile(_) => todo!(),
+                              winit::event::WindowEvent::HoveredFile(_) => todo!(),
+                              winit::event::WindowEvent::HoveredFileCancelled => todo!(),
+                              winit::event::WindowEvent::ReceivedCharacter(_) => todo!(),
+                              winit::event::WindowEvent::Focused(_) => todo!(),
+                              winit::event::WindowEvent::KeyboardInput { device_id, input, is_synthetic } => todo!(),
+                              winit::event::WindowEvent::ModifiersChanged(_) => todo!(),
+                              winit::event::WindowEvent::Ime(_) => todo!(),
+                              winit::event::WindowEvent::CursorMoved { device_id, position, modifiers } => todo!(),
+                              winit::event::WindowEvent::CursorEntered { device_id } => todo!(),
+                              winit::event::WindowEvent::CursorLeft { device_id } => todo!(),
+                              winit::event::WindowEvent::MouseWheel { device_id, delta, phase, modifiers } => todo!(),
+                              winit::event::WindowEvent::MouseInput { device_id, state, button, modifiers } => todo!(),
+                              winit::event::WindowEvent::TouchpadMagnify { device_id, delta, phase } => todo!(),
+                              winit::event::WindowEvent::SmartMagnify { device_id } => todo!(),
+                              winit::event::WindowEvent::TouchpadRotate { device_id, delta, phase } => todo!(),
+                              winit::event::WindowEvent::TouchpadPressure { device_id, pressure, stage } => todo!(),
+                              winit::event::WindowEvent::AxisMotion { device_id, axis, value } => todo!(),
+                              winit::event::WindowEvent::Touch(_) => todo!(),
+                              winit::event::WindowEvent::ScaleFactorChanged { scale_factor, new_inner_size } => todo!(),
+                              winit::event::WindowEvent::ThemeChanged(_) => todo!(),
+                              winit::event::WindowEvent::Occluded(_) => todo!(),
+                          }
+                        
+            },
+            Event::MainEventsCleared => {
+                // Capture the screen
+                let frame = capturer.capture_frame()?;
+
+                // Recognize text using Tesseract
+                let text = tess_api.recognize(&frame)?;
+
+                // Translate the text
+                let translation = translate_text(&client, &text,&api_key).await.unwrap_or_else(|_| String::from("Translation failed"));
+            // Update the overlay window content with the translated text
+            // ...
+
+            // Redraw the window overlay
+            // ...
+         }
+         _ => (),
+     }
+ 
+     *control_flow = ControlFlow::Wait;
+ });
+ 
+ Ok(())
 }
